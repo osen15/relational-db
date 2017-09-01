b450a5f523bd2c1a20eb15a9f56702877a7aa954
Oleg <senyuholeg@gmail.com>
2017-09-01 14:55:49 +0200

    first commit

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,45 @@
+package lesson2;
+
+public class Demo {
+    public static void main(String[] args) {
+        //java
+        //1.create class with fields
+        //2.create object with some values
+        //3.using it
+
+
+
+        //db
+        //1.create table with columns
+        //2.insert some date in the table
+        //3.using it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
