52b2a7d1f3fdc918ff175b042208f24395a00712
Oleg <senyuholeg@gmail.com>
2018-03-21 12:16:32 +0100

    add SQLs and java files

--- src/lesson6/Demo.java
+++ src/lesson6/Demo.java
@@ -1,10 +1,18 @@
 package lesson6;
 
+
+import java.util.*;
+
+
 public class Demo {
     public static void main(String[] args) {
-        Author author = new Author(105, "test");
-        Article article = new Article(1, "Header test", "Text test", author);
+       // Author author = new Author(105, "test");
+       // Article article = new Article(1, "Header test", "Text test", author);
 
+        List<Integer> integers = new LinkedList<>();
+        List<Double> doubles = new LinkedList<>();
+        System.out.println(integers.getClass());
+        System.out.println(doubles.getClass());
 
 
     }
